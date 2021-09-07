5a1647796ece885c892c51fea42b56a6d6cd9846
itechsenior <itechsenior@gmail.com>
2021-09-07 08:16:20 -0700

    created initial project

---
+++ famly-client/src/config.js
@@ -0,0 +1,5 @@
+export const credentials = {
+  accessToken: "234ffdb8-0889-4be3-b096-97ab1679752c",
+  groupId: "11fc220c-ebba-4e55-9346-cd1eed714620",
+  institutionId: "fb6c8114-387e-4051-8cf7-4e388a77b673",
+};
